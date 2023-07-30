0be7f9ca5ebc2494fef8aa2d5a5e51e603fc9f40
Joshua Lim <joshua.lim@hotmail.ch>
2023-07-30 02:13:03 +0200

    Trying to implement PWA with (fail)

--- vite.config.ts
+++ vite.config.ts
@@ -6,10 +6,41 @@ import { VitePWA } from "vite-plugin-pwa";
 export default defineConfig({
   plugins: [
     react(),
+
     VitePWA({
-      registerType: "autoUpdate",
-      devOptions: {
-        enabled: true,
+      registerType: "prompt",
+      includeAssets: ["favicon.ico", "apple-touch-icon.png", "masked-icon.svg"],
+      manifest: {
+        theme_color: "#f69435",
+        background_color: "#f69435",
+        display: "standalone",
+        scope: "/",
+        start_url: "/",
+        short_name: "vite test",
+        description: "testing vite pwa",
+        name: "vite test",
+        icons: [
+          {
+            src: "/android-chrome-192x192.png",
+            sizes: "192x192",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "256x256",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "384x384",
+            type: "image/png",
+          },
+          {
+            src: "/android-chrome-512x512.png",
+            sizes: "512x512",
+            type: "image/png",
+          },
+        ],
       },
     }),
   ],
