f164b620f24107e0dd377ff0d38d24a7123d5b0a
Joshua Lim <joshua.lim@hotmail.ch>
2023-11-26 14:21:51 +0100

    changed hardcoded register form in to a real form

--- src/components/RegisterSite/RegisterForm.tsx
+++ src/components/RegisterSite/RegisterForm.tsx
@@ -1,23 +1,40 @@
-import { FunctionComponent, useCallback } from "react";
+import { FunctionComponent, useCallback, useState } from "react";
 import { useNavigate } from "react-router-dom";
 import styles from "./RegisterForm.module.css";
 
 const RegisterForm: FunctionComponent = () => {
   const navigate = useNavigate();
 
-  const onRegisterButtonContainerClick = useCallback(() => {
-    navigate("/status-site");
-  }, [navigate]);
+  const [userName, setUserName] = useState<string>("");
+  const [email, setEmail] = useState<string>("");
+  const [password, setPassword] = useState<string>("");
+  const handleSubmit = async (
+    event: React.FormEvent<HTMLFormElement>
+  ): Promise<void> => {
+    event.preventDefault();
+
+    try {
+      // const response = await axios.post('http://localhost:8080/auth/register', { username, email, password });
+      // console.log(response.data);
+      console.log(userName, email, password);
+      navigate("/");
+    } catch (error) {
+      console.error("There was an error!", error);
+    }
+  };
+
 
   return (
-    <form className={styles.registerForm}>
+    <form className={styles.registerForm} onSubmit={handleSubmit}>
       <div className={styles.registerInput}>
         <div className={styles.username}>
           <i className={styles.usernameHeadline}>USERNAME</i>
           <input
             className={styles.usernameInput}
-            placeholder="Username"
             type="text"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+            placeholder="Username"
             required
           />
         </div>
@@ -25,8 +42,10 @@ const RegisterForm: FunctionComponent = () => {
           <i className={styles.usernameHeadline}>E-MAIL ADRESS</i>
           <input
             className={styles.usernameInput}
-            placeholder="E-Mail Adress"
             type="email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+            placeholder="E-mail Adress"
             required
           />
         </div>
@@ -34,15 +53,16 @@ const RegisterForm: FunctionComponent = () => {
           <i className={styles.usernameHeadline}>PASSWORD</i>
           <input
             className={styles.usernameInput}
-            placeholder="Password"
             type="password"
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+            placeholder="Password"
             required
           />
         </div>
       </div>
       <div
         className={styles.registerButton}
-        onClick={onRegisterButtonContainerClick}
       >
         <button className={styles.button} type="submit">
           <b className={styles.registerT}>SIGN UP</b>
