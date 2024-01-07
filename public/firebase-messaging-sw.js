8b74a2c4a455ff9f1b8bc9591f7f06d00ae7a68e
Joshua Lim <joshua.lim@hotmail.ch>
2024-01-08 00:44:12 +0100

    Update Firebase messaging service worker

--- public/firebase-messaging-sw.js
+++ public/firebase-messaging-sw.js
@@ -1,20 +1,18 @@
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/10.7.1/firebase-messaging.js');
 
-import { getMessaging, onBackgroundMessage } from 'firebase/messaging/sw';
 
 // Initialize the Firebase app in the service worker by passing the generated config.
 firebase.initializeApp({
     apiKey: "AIzaSyD4PHHoILVqF5l30XBrnDKf59xVvbGfg-M",
     authDomain: "gizapp-1712.firebaseapp.com",
     projectId: "gizapp-1712",
-    storageBucket: "gizapp-1712.appspot.com",
     messagingSenderId: "419851624828",
     appId: "1:419851624828:web:d6f2fb3259c86f29ae84f9"
 });
 
-// Initialize Firebase
-
-
-const messaging = getMessaging();
+// Retrieve an instance of Firebase Messaging so that it can handle background messages.
+const messaging = firebase.messaging();
 
 onBackgroundMessage(messaging, (payload) => {
     console.log('[firebase-messaging-sw.js] Received background message ', payload);
