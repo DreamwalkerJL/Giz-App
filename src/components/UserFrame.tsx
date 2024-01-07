60f614a8f3078f0bdb83c345d520f90e349d93d2
Joshua Lim <joshua.lim@hotmail.ch>
2024-01-07 20:45:55 +0100

    Commented out unused function getImagePath in UserFrame component

--- src/components/UserFrame.tsx
+++ src/components/UserFrame.tsx
@@ -68,10 +68,10 @@ const UserFrame: React.FC<UserFrameProps> = ({ gizComplete }) => {
     setHoveredUserId(null);
   };
 
-  const getImagePath = (profilePicture: string) => {
-    const folder = windowWidth > 800 ? 'ImageUrlsDesktop' : 'ImageUrlsMobile';
-    return `/public/${folder}/${profilePicture}`;
-};
+//   const getImagePath = (profilePicture: string) => {
+//     const folder = windowWidth > 800 ? 'ImageUrlsDesktop' : 'ImageUrlsMobile';
+//     return `/public/${folder}/${profilePicture}`;
+// };
 
 
   return (
