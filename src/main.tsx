340c0a379e8f02ff67fb9af0ba0c4942698321b9
Joshua Lim <joshua.lim@hotmail.ch>
2024-01-11 21:41:16 +0100

    firebase testing 0.0.1

--- src/main.tsx
+++ src/main.tsx
@@ -10,27 +10,27 @@ import { AnimatePresence } from "framer-motion";
 import { GizDataProvider } from "./components/GizDataContext.tsx";
 
 // Register the service worker
-// if ('serviceWorker' in navigator) {
-//   navigator.serviceWorker.register('../firebase-messaging-sw.js')
-//     .then(function(registration) {
-//       console.log('Registration successful, scope is:', registration.scope);
+if ('serviceWorker' in navigator) {
+  navigator.serviceWorker.register('../firebase-messaging-sw.js')
+    .then(function(registration) {
+      console.log('Registration successful, scope is:', registration.scope);
       
-//       // Add an event listener to handle messages from the service worker
-//       navigator.serviceWorker.addEventListener('message', function(event) {
-//         console.log('Received a message from service worker: ', event.data);
-//         if (event.data.action === 'accept') {
-//           // Handle the accept action
-//           // Call the function that handles the accept mutation
-//         } else if (event.data.action === 'decline') {
-//           // Handle the decline action
-//           // Call the function that handles the decline mutation
-//         }
-//       });
+      // Add an event listener to handle messages from the service worker
+      navigator.serviceWorker.addEventListener('message', function(event) {
+        console.log('Received a message from service worker: ', event.data);
+        if (event.data.action === 'accept') {
+          // Handle the accept action
+          // Call the function that handles the accept mutation
+        } else if (event.data.action === 'decline') {
+          // Handle the decline action
+          // Call the function that handles the decline mutation
+        }
+      });
 
-//     }).catch(function(err) {
-//       console.log('Service worker registration failed, error:', err);
-//     });
-// }
+    }).catch(function(err) {
+      console.log('Service worker registration failed, error:', err);
+    });
+}
 ReactDOM.createRoot(document.getElementById("root")!).render(
   <React.StrictMode>
     <AuthProvider>
